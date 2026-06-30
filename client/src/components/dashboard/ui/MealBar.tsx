52e67d066d46c814e9efca6a774367b0657baa6b
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-06-30 13:27:44 +0200

    Fixed dynamic tailwind color classes

--- client/src/components/dashboard/ui/MealBar.tsx
+++ client/src/components/dashboard/ui/MealBar.tsx
@@ -7,11 +7,29 @@ interface MealBarProps {
 }
 
 const MealBar = ({ meal, food, calories }: MealBarProps) => {
-  const color = meal === 'breakfast' ? 'indigo' : meal === 'lunch' ? 'yellow' : meal === 'dinner' ? 'green' : 'orange'
+  // const color = meal === 'breakfast' ? 'indigo' : meal === 'lunch' ? 'yellow' : meal === 'dinner' ? 'green' : 'orange'
+  const colorMap = {
+    breakfast: {
+      background: "bg-indigo-300/30",
+      text: "text-indigo-900"
+    },
+    lunch: {
+      background: "bg-yellow-300/30",
+      text: "text-yellow-900"
+    },
+    dinner: {
+      background: "bg-green-300/30",
+      text: "text-green-900"
+    },
+    snack: {
+      background: "bg-orange-300/30",
+      text: "text-orange-900"
+    },
+  }
   const emoji = meal === 'breakfast' ? "🍳" : meal === 'lunch' ? "🍲" : meal === 'dinner' ? "🥪" : "🍫"
   
   return (
-    <div className={`w-11/12 h-20 bg-${color}-300/30 rounded-lg flex justify-between items-center px-5`}>
+    <div className={`w-11/12 h-20 ${ colorMap[meal].background } rounded-lg flex justify-between items-center px-5`}>
       <div className="flex items-center text-2xl gap-2">
         <h2>{ emoji }</h2>
         <div>
@@ -19,9 +37,9 @@ const MealBar = ({ meal, food, calories }: MealBarProps) => {
           <h4 className="text-sm -mt-2">{ food }</h4>
         </div>
       </div>
-      <div className="text-right">
-        <h3 className={`text-2xl font-bold text-${color}-900`}>{ calories || 0 }</h3>
-        <h3 className={`text-md text-${color}-900 text-right`}>kcal</h3>
+      <div className={`text-right ${ colorMap[meal].text }`}>
+        <h3 className="text-2xl font-bold">{ calories || 0 }</h3>
+        <h3 className="text-md text-right">kcal</h3>
       </div>
     </div>
   )
