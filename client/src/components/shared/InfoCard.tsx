8e806d83bc57e57c9fc9e6ac6e0a2e4b3b8ac875
Bogdan Petrovic <bogdan.pettrovic@gmail.com>
2026-08-05 02:25:12 +0200

    Reduced the font size for longer texts and shortened the delay in the demo mock functions

--- client/src/components/shared/InfoCard.tsx
+++ client/src/components/shared/InfoCard.tsx
@@ -15,7 +15,15 @@ const InfoCard = ({ bgColor, Icon, label, value }: InfoCardProps) => {
       </div>
       <div className="flex flex-col">
         <h5 className="text-muted text-2xl">{ label }</h5>
-        <h3 className={`font-bold text-4xl ${ label === 'Goal' && 'capitalize' }`}>{ value }</h3>
+        <h3 
+          className={`
+            font-bold 
+            ${ typeof value === 'string' && value.length > 12 ? 'text-2xl' : 'text-4xl' } 
+            ${ label === 'Goal' && 'capitalize' }
+          `}
+        >
+          { value }
+        </h3>
       </div>
     </div>
   )
